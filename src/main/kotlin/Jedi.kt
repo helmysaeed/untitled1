9530c2d7071a8eb741d9ad170fdae692b5c7a708
helmysaeed <helmy.saeed@gmail.com>
2021-04-08 12:31:30 +0300

    add save button to inventory
    add new item to databse

--- src/main/kotlin/Jedi.kt
+++ src/main/kotlin/Jedi.kt
@@ -4,10 +4,15 @@ import org.litote.kmongo.findOne
 import org.litote.kmongo.getCollection
 
 public data class Users(val name: String, val passWord: String)
+public data class Item(val name: String)
 
 val client = KMongo.createClient() //get com.mongodb.MongoClient new instance
 val database = client.getDatabase("test") //normal java driver usage
-val col = database.getCollection<Users>() //KMongo extension method
+val col = database.getCollection<Users>()
+val colItem = database.getCollection<Item>()
+
+
+//KMongo extension method
 //here the name of the collection by convention is "jedi"
 //you can use getCollection<Jedi>("otherjedi") if the collection name is different
 
